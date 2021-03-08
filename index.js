70795531e4bf522d98017c80920bf213e43dbf79
Charles Lai <charlesangeloplai@gmail.com>
2021-03-08 17:11:21 -0500

    commit tree 63c41996a66e661cc876537e74f72ac447037b0e

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    if (!Array.isArray(this.sides)) return;
+    let sum = 0;
+
+    for (let val of this.ided) {
+      sum += val;
+    }
+
+    return sum;
+  }
+}
+
+class Triange extends Polygon {
+  get isVaid() {
+    if (!Array.isArray(this.sides)) return;
+    return this.sides.length != 3 ? true : false;
+  }
+}
+
+class Square extends Polygon {
+  get isVaid() {
+    if (!Array.isArray(this.sides)) return;
+    return this.sides.length != 4 ? true : false;
+  }
+
+  get area() {
+    if (!Array.isArray(this.sides)) return;
+    if (this.count !== 4) return;
+    return this.sides[0] * this.sides[0];
+  }
+}
